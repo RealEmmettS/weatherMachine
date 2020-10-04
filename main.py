b4f5f28e091edba7691b444f75ccff52a0e71090
Emmett S <emmett0502@gmail.com>
2020-10-04 21:56:23 +0000

    init

--- main.py
+++ main.py
@@ -1,41 +1,83 @@
+import os
+clear = lambda: os.system('clear')
+
+import art
+from art import *
+
+import time
+from time import sleep
+
 import requests
 from bs4 import BeautifulSoup
-import geocoder
 
-#Get Location
-usrLoc = input("Where are you right now? ")
-g = geocoder.google('Mountain View, CA')
+import geopy
+from geopy.geocoders import Nominatim
 
-lat = g.lat
-lon = g.lng
+###################################################
+
+Art = text2art("ES Weather")
+print(Art, "\n")
+
+def getWeatherNow():
+  #Get Location
+  usrLoc = input("Enter location to check: ")
+  geolocator = Nominatim(user_agent="ES_Weather-Machine")
+  location = geolocator.geocode(usrLoc)
+  lat = location.latitude
+  lon = location.longitude
+
+  print ("lat: ", lat, "\nlon: ", lon, "\n", "\nAccessing federal weather data...", "\n\n\n")
+  time.sleep(2)
 
 
-print (g.latlng)
+  #weather data NO APIs!!!!
+  requestURLwithLatLon = ("http://forecast.weather.gov/MapClick.php?lat="+str(lat)+"&lon="+str(lon))
+  page = requests.get(requestURLwithLatLon)
+  soup = BeautifulSoup(page.content, 'html.parser')
+  seven_day = soup.find(id="seven-day-forecast")
+  forecast_items = seven_day.find_all(class_="tombstone-container")
+  tonight = forecast_items[0]
+  #print(tonight.prettify())
 
-print (str(lon) + "\n\n\n")
+
+  print ("BRIEF WEATHER:")
+  #breaking down HTML into chunks of valuable information
+  timePeriod = tonight.find(class_="period-name").get_text()
+  short_desc = tonight.find(class_="short-desc").get_text()
+  lowTemp = tonight.find(class_="temp").get_text()
+  print(timePeriod)
+  print(short_desc)
+  print(lowTemp)
+
+  print ("\n")
+
+
+  print ("DETAILED WEATHER:")
+  #More detailed info can be found in the img title
+  img = tonight.find("img")
+  desc = img['title']
+  print(desc)
+
+  print ("\n\n\n")
 
 
 
 
-#weather data NO APIs!!!!
-page = requests.get("http://forecast.weather.gov/MapClick.php?lat=33.138230&lon=-96.882751")
-soup = BeautifulSoup(page.content, 'html.parser')
-seven_day = soup.find(id="seven-day-forecast")
-forecast_items = seven_day.find_all(class_="tombstone-container")
-tonight = forecast_items[0]
-#print(tonight.prettify())
 
-#breaking down HTML into chunks of valuable information
-timePeriod = tonight.find(class_="period-name").get_text()
-short_desc = tonight.find(class_="short-desc").get_text()
-lowTemp = tonight.find(class_="temp").get_text()
-print(timePeriod)
-print(short_desc)
-print(lowTemp)
 
-print ("\n\n")
 
-#More detailed info can be found in the img title
-img = tonight.find("img")
-desc = img['title']
-print(desc)
+
+#Program begins here
+getWeatherNow()
+
+
+
+repeat = input("Would you like to access another location? (y/n) ")
+if repeat == "y":
+  clear()
+  time.sleep(2.5)
+  getWeatherNow()
+elif repeat == "n":
+  print("Ok.")
+else:
+  print("Your input was not understood. Please refresh this page.")
